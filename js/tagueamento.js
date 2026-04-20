1687ec6e24b3b95ec79d8f216171bc9fa2a22854
Alessandro Paveloski <sandropave@gmail.com>
2026-04-20 14:21:26 -0300

    Refactor dataLayer push syntax for click event

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -3,9 +3,9 @@ document.querySelectorAll('.card-montadoras').forEach(function(card) {
     window.dataLayer = window.dataLayer || [];
     window.dataLayer.push({
       event: 'click',
-      page_location: window.location.href,
-      element_name:  this.getAttribute('data-name'), // "Lorem", "Ipsum" ou "Dolor"
-      element_group: 'ver_mais'
+      page_location:  window.location.href,
+      element_name:   this.getAttribute('data-name'),
+      element_group:  'ver_mais'
     });
   });
 });
