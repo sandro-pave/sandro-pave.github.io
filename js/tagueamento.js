d1650b2851fea72cfc082ec9ad147b0b2fbc34a7
Alessandro Paveloski <sandropave@gmail.com>
2026-04-20 13:58:24 -0300

    Implement click tracking for card elements

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -1,6 +1,11 @@
-// Preencha este arquivo com qualquer código que você necessite para realizar a
-// coleta, desde a biblioteca analytics.js, gtag.js ou o snippet do Google Tag 
-// Manager. No último caso, não é necessário implementar a tag <noscript>.
-// O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
-// para fazer a sua coleta.
-// Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
+document.querySelectorAll('.card-montadoras').forEach(function(card) {
+  card.addEventListener('click', function() {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      event: 'click',
+      page_location: window.location.href,
+      element_name:  this.getAttribute('data-name'), // "Lorem", "Ipsum" ou "Dolor"
+      element_group: 'ver_mais'
+    });
+  });
+});
